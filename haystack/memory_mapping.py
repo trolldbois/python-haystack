607a8ffe1dea804294649afd3eeace9c22c99809
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-10-13 18:26:18 -0400

    nicer

--- haystack/memory_mapping.py
+++ haystack/memory_mapping.py
@@ -478,10 +478,24 @@ class Mappings:
     if mmap is None:
       raise IndexError('No mmap of pathname %s'%(pathname))
     return mmap
+
+  def getMmapForAddr(self, vaddr):
+    for m in self.mappings:
+      if vaddr in m:
+        return m
+    return False
+
   def getHeap(self):
     return self.getMmap('[heap]')  
   def getStack(self):
     return self.getMmap('[stack]')  
+
+  def __contains__(self, vaddr):
+    for m in self.mappings:
+      if vaddr in m:
+        return True
+    return False
+
   def __len__(self):
     return len(self.mappings)
   def __getitem__(self, i):
