9bca15339f60d0a5e8fddc52df48057f0993be1f
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-10-18 23:08:12 -0400

    utils.py

--- haystack/reverse/cache_utils.py
+++ haystack/reverse/cache_utils.py
@@ -16,19 +16,4 @@ import logging
 
 log = logging.getLogger('cache_utils')
 
-def int_array_cache(filename):
-  if os.access(filename,os.F_OK):
-    # load
-    f = file(filename,'r')
-    nb = os.path.getsize(f.name)/4 # simple TODO 
-    my_array = array.array('L')
-    my_array.fromfile(f,nb)
-    return my_array
-  return None
-
-def int_array_save(filename, lst):
-  my_array = array.array('L')
-  my_array.extend(lst)
-  my_array.tofile(file(filename,'w'))
-  return my_array
 
