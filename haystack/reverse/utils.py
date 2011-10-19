a01b1f47c13acbcb57ce35223d06c68d59cbed2a
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-10-18 23:08:25 -0400

    utils.py cache_utils.py

--- haystack/reverse/utils.py
+++ haystack/reverse/utils.py
@@ -4,11 +4,34 @@
 # Copyright (C) 2011 Loic Jaquemet loic.jaquemet+python@gmail.com
 #
 
+'''
+This module holds some basic utils function.
+'''
+
+__author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
+
 import logging
 import numpy
+import os
+import array
 
 log = logging.getLogger('utils')
 
+def int_array_cache(filename):
+  if os.access(filename,os.F_OK):
+    # load
+    f = file(filename,'r')
+    nb = os.path.getsize(f.name)/4 # simple TODO 
+    my_array = array.array('L')
+    my_array.fromfile(f,nb)
+    return my_array
+  return None
+
+def int_array_save(filename, lst):
+  my_array = array.array('L')
+  my_array.extend(lst)
+  my_array.tofile(file(filename,'w'))
+  return my_array
 
 
 def closestFloorValueNumpy(val, lst):
