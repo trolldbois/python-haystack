378d9386a8d6324ca4e7ce95be5a1f95031fb726
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-11-05 16:33:17 -0400

    a DiGraph is better

--- haystack/reverse/reversers.py
+++ haystack/reverse/reversers.py
@@ -322,9 +322,9 @@ class PointerGraphReverser(StructureOrientedReverser):
   
   def _reverse(self, context):
     import networkx
-    import code
-    code.interact(local=locals())
-    graph = networkx.Graph()
+    #import code
+    #code.interact(local=locals())
+    graph = networkx.DiGraph()
     graph.add_nodes_from(context.structures.keys()) # we only need the addresses...
     log.info('[+] Graph - added %d nodes'%(graph.number_of_nodes()))
     t0 = time.time()
@@ -368,6 +368,23 @@ def refreshOne(context, ptr_value):
   #resolvePointers
   return mystruct
   
+def save_graph_headers(context, graph, fname):
+  fout = file(fname,'w')
+  towrite = []
+  structs = [context.structures[int(addr)] for addr in graph.nodes()]
+  for anon in structs:
+    towrite.append(anon.toString())
+    if len(towrite) >= 10000:
+      try:
+        fout.write('\n'.join(towrite) )
+      except UnicodeDecodeError, e:
+        print 'ERROR on ',anon
+      towrite = []
+      fout.flush()
+  fout.write('\n'.join(towrite) )
+  fout.close()
+  return
+
 def save_headers(context):
   ''' structs_addrs is sorted '''
   fout = file(Config.getCacheFilename(Config.CACHE_GENERATED_PY_HEADERS_VALUES, context.dumpname),'w')
