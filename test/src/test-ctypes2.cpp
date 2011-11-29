e9bf825a38be41abbf7ae9e497ab7cc2fa85ee9e
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-11-28 23:24:25 -0500

    test cases

--- test/src/test-ctypes2.cpp
+++ test/src/test-ctypes2.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <iostream>
 
+extern "C" {
+#include <dlfcn.h>
+
+}
 #ifdef PYTHON_BUILD
 #endif
 
@@ -27,26 +31,35 @@ struct sD : sB {
 
 // classes
 class cA {
+  public:
   int a;
+  //cA() : a(0x1){  };
+  cA(int _a=0x1) : a(_a){  };
+  
 };
 
 class cB : cA {
+  public:
   unsigned int b;
+  cB(int _b=0x22) : b(_b) { cA(0x2);};
 };
 
 class cC : cB {
-  private:
+  public:
   unsigned int c;
+  cC(int _c=0x333) : c(_c) {cB(0x3);};
 };
 
 class cD : cB {
   public:
   unsigned int d;
+  cD(int _d=0x4) : d(_d) {cB(0x4);};
 };
 
 class cE : cD, cC {
   public:
   unsigned int e;
+  cE() : e(0x5) {cD(0x5);};
 };
 
 
@@ -60,11 +73,18 @@ int test_classes()
   cD * d = new cD();
   cE * e = new cE();
   
-  printf(" a is at 0x%x , size: %d \n", (unsigned int) a, sizeof(cA));
-  printf(" b is at 0x%x , size: %d \n", (unsigned int) b, sizeof(cB));
-  printf(" c is at 0x%x , size: %d \n", (unsigned int) c, sizeof(cC));
-  printf(" d is at 0x%x , size: %d \n", (unsigned int) d, sizeof(cD));
-  printf(" e is at 0x%x , size: %d \n", (unsigned int) e, sizeof(cE));
+  //a->a = 0x01 ;
+  //b->a = 0x02;
+  //b->b = 0x22;
+  //c->a = 0x03;
+  //c->b = 0x33;
+  //c->c = 0x0333;
+  
+  printf(" a is at 0x%x size: %d \n", (unsigned int) a, sizeof(cA));
+  printf(" b is at 0x%x size: %d \n", (unsigned int) b, sizeof(cB));
+  printf(" c is at 0x%x size: %d \n", (unsigned int) c, sizeof(cC));
+  printf(" d is at 0x%x size: %d \n", (unsigned int) d, sizeof(cD));
+  printf(" e is at 0x%x size: %d \n", (unsigned int) e, sizeof(cE));
 
   std::cout << " -- end classes --" << std::endl;
   
@@ -73,15 +93,65 @@ int test_classes()
 
 int test_structs()
 {
+  std::cout << " -- structs --" << std::endl;
+
   sA * a = (sA * ) malloc(sizeof(sA));
   sB * b = (sB * ) malloc(sizeof(sB));
   sC * c = (sC * ) malloc(sizeof(sC));
   sD * d = (sD * ) malloc(sizeof(sD));
+
+  printf(" a is at 0x%x size: %d \n", (unsigned int) a, sizeof(sA));
+  printf(" b is at 0x%x size: %d \n", (unsigned int) b, sizeof(sB));
+  printf(" c is at 0x%x size: %d \n", (unsigned int) c, sizeof(sC));
+  printf(" d is at 0x%x size: %d \n", (unsigned int) d, sizeof(sD));
+
+  std::cout << " -- end structs --" << std::endl;
+  
+  return 0;
 }
 
 int main(){
+  int a;
+  void *handle;
+
+  printf("START\n");
+
+  fflush(stdout);
+  //test_structs();
+  //test_classes();   
+  a = getchar();
+  handle = dlopen ("libQtCore.so", RTLD_NOW|RTLD_GLOBAL);
+  printf("OPEN libQtCore.so\n");
+  fflush(stdout);
+  if (handle == NULL){
+    fprintf (stderr, "cannot load: lib.so\n");
+    return -1;  
+  }
   
-  test_structs();
-  test_classes();
+  //test_structs();
+  //test_classes();   
+  a = getchar();
+  handle = dlopen ("libQtSvg.so", RTLD_NOW|RTLD_GLOBAL);
+  printf("OPEN libQtSvg.so\n");
+  fflush(stdout);
+  if (handle == NULL){
+    fprintf (stderr, "cannot load: lib.so\n");
+    return -1;  
+  }
+  
+  //test_structs();
+  //test_classes();   
+  a = getchar();
+  handle = dlopen ("libQtGui.so", RTLD_NOW|RTLD_GLOBAL);
+  printf("OPEN libQtGui.so\n");
+  fflush(stdout);
+  if (handle == NULL){
+    fprintf (stderr, "cannot load: lib.so\n");
+    return -1;  
+  }
+
+  a = getchar();
+  printf("END\n");
+  fflush(stdout);
   return 0;
 }
