e8ea28f08f6d0bfd81d76f6817806e4e8ec6048b
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-01-08 12:52:25 -0500

    pydoc + moving the output dir

--- haystack/config.py
+++ haystack/config.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2011 Loic Jaquemet loic.jaquemet+python@gmail.com
-#
 
-__author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
+"""Default configuration for filenames, output directories and such."""
 
 import logging
 import os
 
-from haystack.utils import Dummy
+__author__ = "Loic Jaquemet"
+__copyright__ = "Copyright (C) 2012 Loic Jaquemet"
+__license__ = "GPL"
+__maintainer__ = "Loic Jaquemet"
+__email__ = "loic.jaquemet+python@gmail.com"
+__status__ = "Production"
 
 log = logging.getLogger('config')
 
-OUTPUTDIR=os.path.expanduser('~/Compil/outputs/malloc/')
+OUTPUTDIR=os.path.expanduser('~/outputs/malloc/') # .haystack-tmp ?
 
 class ConfigClass():
+  """Project-wide config class. """
   def __init__(self, outputDir=OUTPUTDIR):
     self.cacheDir = os.path.normpath(outputDir)
     self.imgCacheDir = os.path.sep.join([self.cacheDir,'img'])
@@ -37,15 +40,20 @@ class ConfigClass():
     self.CACHE_GRAPH = '.gexf'
     self.DIFF_PY_HEADERS='.diff_headers'
   
-  ''' 
-  @param typ: one of Config.CACHE_XX types'''
   def getCacheFilename(self, typ, dumpfilename):
+    '''Returns a filename for caching a type of data based on the dump filename.
+  
+    typ: one of Config.CACHE_XX types.
+    dumpfilename: the dump file name.
+    '''
     root = os.path.basename(dumpfilename)
     return os.path.sep.join([self.cacheDir, root+typ])
 
-  ''' 
-  @param typ: one of Config.CACHE_XX types'''
   def getStructsCacheDir(self, dumpfilename):
+    '''Returns a dirname for caching the structures based on the dump filename.
+  
+    dumpfilename: the dump file name.
+    '''
     root = os.path.basename(dumpfilename)
     return self.getCacheFilename(self.CACHE_STRUCT_DIR, root)
 
