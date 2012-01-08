5f05536be11d69c5b21505700ca6cd637d7366dd
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-01-08 17:54:29 -0500

    unittest

---
+++ test/run_src_app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Launch our test unit applications."""
+
+import logging
+import os
+import subprocess
+import sys
+
+__author__ = "Loic Jaquemet"
+__copyright__ = "Copyright (C) 2012 Loic Jaquemet"
+__email__ = "loic.jaquemet+python@gmail.com"
+__license__ = "GPL"
+__maintainer__ = "Loic Jaquemet"
+__status__ = "Production"
+
+
+TESTS={ "test1": "test-ctypes1",
+  "test2": "test-ctypes2",
+  "test3": "test-ctypes3",
+}
+
+
+def run_app_test(testName, stdout=sys.stdout):
+  if testName not in TESTS:
+    raise ValueError("damn, please choose testName in %s"%(TESTS.keys()))
+  appname = TESTS[testName]
+  srcDir = os.path.sep.join([os.getcwd(), 'test', 'src'])
+  tgt = os.path.sep.join([srcDir, appname])
+  return subprocess.Popen([tgt], stdout=stdout)
+
+
+
