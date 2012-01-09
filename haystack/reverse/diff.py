54d850771db1f61bafd793b9fab9dcd42aae5164
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-01-08 23:15:10 -0500

    working towards something fast

--- haystack/reverse/diff.py
+++ haystack/reverse/diff.py
@@ -34,20 +34,38 @@ log = logging.getLogger('diff')
 def make(opts):
   log.info('[+] Loading context of %s'%(opts.dump1))
   context = reversers.getContext(opts.dump1) #'../../outputs/skype.1.a') # TODO 
+  # refresh
+  if len(context.structures) != len(context.structures_addresses):
+    log.info('[+] Refreshing from %d structures cached'%( len(context.structures) ))
+    mallocRev = MallocReverser()
+    context = mallocRev.reverse(context)
+    mallocRev.check_inuse(context)
+    log.info('[+] Final %d structures from malloc blocs'%( len(context.structures) ))
+
+  
   heap1 = context.mappings.getHeap()
   log.info('[+] Loading mappings of %s'%(opts.dump2))
   newmappings = dump_loader.load( opts.dump2)  
   heap2 = newmappings.getHeap()
   log.info('[+] finding diff values with %s'%(opts.dump2))
-  offsets = findDiffsOffsets_mappings(heap1, heap2, heap1.start)
+  #offsets = findDiffsOffsets_mappings(heap1, heap2, heap1.start)
+  #offsets = diff_iterator(heap1, heap2, heap1.start)
+  offsets = cmd_cmp(heap1, heap2, heap1.start)
   
   # now compare with structures addresses
-  structures = set()
-  for offset in offsets:
+  structures = []
+  log.info('[+] Looking at %d offsets'%( len(offsets) ))
+  st = []
+  saved=0
+  for offsets_found, offset in enumerate(offsets):
+    if offset in st: # last structure could hold this offset too
+      saved+=1
+      continue
     vaddr, pos = utils.closestFloorValueNumpy(offset, context.structures_addresses)
     st = context.structures[vaddr]
-    structures.add(st)
-  log.info('[+] On %d diffs, found %d structs with different values. Outputing to file (will be long-ish)'%( len(offsets), len(structures) ))
+    structures.append(st)
+  log.info('[+] On %d diffs, found %d structs with different values. saved: %d'%(offsets_found, len(structures), saved))
+  log.info('[+] Outputing to file (will be long-ish)')
 
   # print original struct in one file, diffed struct in the other
   d1out = config.Config.getCacheFilename(config.Config.DIFF_PY_HEADERS, opts.dump1) 
@@ -139,7 +157,49 @@ def cmp_recursive(buflen, data1, data2, offset):
   if d1d != d2d:
     offsets.extend( cmp_recursive(mid, d1d, d2d, offset+mid) )
   return offsets
+
+
+def diff_iterator(heap1, heap2, baseOffset):
   
+  def my_iter(heap1, heap2, baseOffset):
+    size1 = len(heap1)
+    size2 = len(heap2)
+    if size1 != size2:
+      log.debug('different sizes')
+    
+    h1 = heap1.getByteBuffer()
+    h2 = heap2.getByteBuffer()
+    offsets = []
+    offset=0
+    for offset in xrange( min(size1,size2)):
+      if h1[offset] != h2[offset]:
+        yield(baseOffset + offset)
+    return  
+  return iter(my_iter(heap1, heap2, baseOffset))
+  
+
+def cmd_cmp(heap1, heap2, baseOffset):
+  # LINUX based system command cmp parsing
+  import subprocess
+  
+  f1 = heap1._memdump.name
+  f2 = heap2._memdump.name
+  
+  offsets = []
+  try:
+    res = subprocess.check_output(['cmp',f1,f2,'-l'])
+  except subprocess.CalledProcessError,e:
+    res = e.output
+  for line in res.split('\n'):
+    cols = line.split(' ')
+    try:
+      while cols[0] == '':
+        cols.pop(0)
+    except:
+      continue
+    offsets.append(int(cols.pop(0)))
+  
+  return offsets
   
   
 def argparser():
