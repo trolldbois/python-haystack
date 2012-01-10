ca4421bc1dd46a4ec6091b33d38435ea8d2dbc9e
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-01-09 20:00:17 -0500

    forgetting bad algo

--- haystack/reverse/diff.py
+++ haystack/reverse/diff.py
@@ -48,8 +48,6 @@ def make(opts):
   newmappings = dump_loader.load( opts.dump2)  
   heap2 = newmappings.getHeap()
   log.info('[+] finding diff values with %s'%(opts.dump2))
-  #offsets = findDiffsOffsets_mappings(heap1, heap2, heap1.start)
-  #offsets = diff_iterator(heap1, heap2, heap1.start)
   addrs = cmd_cmp(heap1, heap2, heap1.start)
   
   # now compare with structures addresses
@@ -115,92 +113,6 @@ def make(opts):
   f1.close()
   f2.close()
   log.info('[+] diffed structures dumped in %s %s'%(d1out, d2out))
-
-def findDiffsOffsets_files(file1, file2, baseOffset):
-  '''Makes a dichotomic search of unequals values in bytebuffers.
-  returns offsets of unequals word value
-  '''
-  size1 = os.fstat(file1.fileno()).st_size
-  size2 = os.fstat(file2.fileno()).st_size
-  if size1 != size2:
-    log.debug('different sizes')
-  d1 = mmap.mmap(file1.fileno(), size1, access=mmap.ACCESS_READ)
-  d2 = mmap.mmap(file2.fileno(), size2, access=mmap.ACCESS_READ)
-  #
-  offsets = []
-  buflen=2**16
-  offset=0
-  while offset < size1 and offset < size2:
-    data1 = d1.read(buflen) 
-    data2 = d2.read(buflen) 
-    if data1 != data2:
-      diffs = cmp_recursive(buflen, data1, data2, baseOffset)
-      offsets.extend(diffs)
-      #log.debug('%d diff at offsets %d'%(len(diffs), offset))
-    offset += buflen
-    baseOffset += buflen
-  log.debug('simple finished with %d diffs'%(len(offsets)))
-  return sorted(offsets)
-
-def findDiffsOffsets_mappings(heap1, heap2, baseOffset):
-  size1 = len(heap1)
-  size2 = len(heap2)
-  if size1 != size2:
-    log.debug('different sizes')
-  #
-  offsets = []
-  buflen=2**16
-  offset=0
-  while offset < size1 and offset < size2:
-    data1 = heap1.readBytes(baseOffset, buflen) 
-    data2 = heap2.readBytes(baseOffset, buflen) 
-    if data1 != data2:
-      diffs = cmp_recursive(buflen, data1, data2, baseOffset)
-      offsets.extend(diffs)
-      #log.debug('%d diff at offsets %d'%(len(diffs), offset))
-    offset += buflen
-    baseOffset += buflen
-  log.debug('simple finished with %d diffs'%(len(offsets)))
-  return sorted(offsets)
-
-'''
-Make a recursive dichotomic comparaison
-'''
-def cmp_recursive(buflen, data1, data2, offset):
-  if buflen < config.Config.WORDSIZE+1: # find word difference
-    return [offset] # 0+offset or mid+offset
-  offsets = []
-  mid = min(min(buflen/2, len(data1) ), len(data2) )
-  #print mid+offset, len(data1)
-  d1g = data1[:mid]
-  d2g = data2[:mid]
-  if d1g != d2g:
-    offsets.extend( cmp_recursive(mid, d1g, d2g, offset) )
-  d1d = data1[mid:]
-  d2d = data2[mid:]
-  if d1d != d2d:
-    offsets.extend( cmp_recursive(mid, d1d, d2d, offset+mid) )
-  return offsets
-
-
-def diff_iterator(heap1, heap2, baseOffset):
-  
-  def my_iter(heap1, heap2, baseOffset):
-    size1 = len(heap1)
-    size2 = len(heap2)
-    if size1 != size2:
-      log.debug('different sizes')
-    
-    h1 = heap1.getByteBuffer()
-    h2 = heap2.getByteBuffer()
-    offsets = []
-    offset=0
-    for offset in xrange( min(size1,size2)):
-      if h1[offset] != h2[offset]:
-        yield(baseOffset + offset)
-    return  
-  return iter(my_iter(heap1, heap2, baseOffset))
-  
 
 def cmd_cmp(heap1, heap2, baseOffset):
   # LINUX based system command cmp parsing
