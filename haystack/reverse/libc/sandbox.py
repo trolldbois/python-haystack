9668219912d999b54ff3575a2cb50aa25ffdd8ee
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-04-16 21:10:21 -0400

    playing with dl_addr

--- haystack/reverse/libc/sandbox.py
+++ haystack/reverse/libc/sandbox.py
@@ -11,11 +11,15 @@ b6f01000-b6f04000 rw-p 0029d000 08:04 3426931    /usr/lib/i386-linux-gnu/libQtCo
 '''
 import struct
 import os
+import sys
 import ctypes
 
 offset = 0xb6b3ef68 - 0xb68b1000
 
 from haystack import memory_mapping, utils
+
+class Dummy():
+  pass
 
 class Dl_info(ctypes.Structure):
   _fields_ = [
@@ -24,129 +28,104 @@ class Dl_info(ctypes.Structure):
   ('dli_sname', ctypes.c_char_p), #Name of nearest symbol with address lower than addr
   ('dli_saddr', ctypes.c_void_p)  #Exact address of symbol named in dli_sname
   ]
-  
 
-info = Dl_info()
+def getMappings():
+  me = Dummy()
+  me.pid = os.getpid()
+  return memory_mapping.readProcessMappings(me)
+
+  
+def test1():
+  info = Dl_info()
+
+  #handle = libdl.dlopen('/usr/lib/libQtCore.so.4.7.2')
+
+  libname = '/usr/lib/libQtCore.so.4.7.2'
+  libname2 = libname[libname.rindex(os.path.sep)+1:libname.index('.so')+3]
+  print libname2
+  libqt = ctypes.CDLL(libname2)
+
+  localmappings = getMappings()
+  qtmaps = [m for m in localmappings if m.pathname is not None and libname2 in m.pathname ]
+
+  myvaddr = qtmaps[0].start+offset
+
+  ret = libdl.dladdr( myvaddr, ctypes.byref(info))
+  print 'filling dlinfo with',libname, info
+
+  signed_addr = libdl.dlsym( 0, 'dladdr', 'xxx')
+  vaddr_dladdr = struct.unpack('L',struct.pack('l', signed_addr) )[0]
+  ret = libdl.dladdr( vaddr_dladdr, ctypes.byref(info))
+  print 'dlsym test', info.dli_sname.string, info.dli_sname.string == 'dladdr'
+
+def test2():
+  # now for the real deal.
+  # we need to emulate ELF dl-addr.c 
+
+
+  print ''
+
+  #
+  #define DL_LOOKUP_ADDRESS(addr) _dl_lookup_address (addr)
+
+  libssl = ctypes.CDLL('/usr/lib/libssl.so.0.9.8')
+  localmappings = getMappings()
+
+  print 'libssl.ssl3_read by id() is @%x'%( id(libssl.ssl3_read) )
+  print localmappings.getMmapForAddr(id(libssl.ssl3_read))
+
+  print ''
+  signed_addr = libssl.dlsym( libssl._handle, 'ssl3_read', 'xxx')
+  fnaddr = struct.unpack('L',struct.pack('l', signed_addr) )[0]
+  print 'libssl.ssl3_read by dlsym is @%x'%(fnaddr)
+  print localmappings.getMmapForAddr(fnaddr)
+
+  info = Dl_info()
+  ret = libdl.dladdr( fnaddr, ctypes.byref(info))
+  print 'dladdr test', info.dli_sname.string, info.dli_sname.string == 'ssl3_read'
+  '''
+libssl.ssl3_read by id() is @9528ecc
+0x0924a000 0x095d1000 rw-p 0x00000000 00:00 0000000 [heap]
+
+libssl.ssl3_read by dlsym is @b6ddd9b0
+0xb6dc2000 0xb6e0c000 r-xp 0x00000000 08:04 7739090 /lib/libssl.so.0.9.8
+dladdr test ssl3_read True
+  '''
+  print ''
+
+  # testing low level
+  # low level call 
+  #(const void *address, Dl_info *info,
+  #	  struct link_map **mapp, const ElfW(Sym) **symbolp)
+  print libdl._dl_addr( fnaddr, ctypes.byref(info), 0, 0)
+  # iterate the struct link_map
+  #for (Lmid_t ns = 0; ns < GL(dl_nns); ++ns)
+  #  for (struct link_map *l = GL(dl_ns)[ns]._ns_loaded; l; l = l->l_next)
+  #    if (addr >= l->l_map_start && addr < l->l_map_end
+	#  && (l->l_contiguous || _dl_addr_inside_object (l, addr)))
+  
+  return
+
+
+def test3():
+  # load local memdump
+  # map all librairies
+  # go through all pointers in librairies
+  # try to dl_addr the pointers by rebasing.
+  from haystack import memory_loader
+  dump = memory_loader.load('/home/jal/outputs/dumps/ssh/ssh.1.dump')
+
 
 libdl = ctypes.CDLL('libdl.so')
-#handle = libdl.dlopen('/usr/lib/libQtCore.so.4.7.2')
 
-libname = '/usr/lib/libQtCore.so.4.7.2'
-libname2 = libname[libname.rindex(os.path.sep)+1:libname.index('.so')+3]
-print libname2
-libqt = ctypes.CDLL(libname2)
+def main(argv):
+  #test1()
+  #test2()
+  test3()
 
-me = utils.Dummy()
-me.pid = os.getpid()
-localmappings = memory_mapping.readProcessMappings(me)
-qtmaps = [m for m in localmappings if m.pathname is not None and libname2 in m.pathname ]
-
-myvaddr = qtmaps[0].start+offset
-
-ret = libdl.dladdr( myvaddr, ctypes.byref(info))
-print info
-
-signed_addr = libdl.dlsym( 0, 'dladdr', 'xxx')
-vaddr_dladdr = struct.unpack('L',struct.pack('l', signed_addr) )[0]
-ret = libdl.dladdr( vaddr_dladdr, ctypes.byref(info))
-print info.dli_sname.string
-
-
-import pattern 
-pattern.main('outputs/skype.1.a outputs/skype.2.a outputs/skype.3.a'.split())
-mapper = pattern.mapper
-
-unresolved = pattern.mapper.unresolved
-resolved = pattern.mapper.resolved
-
-p1 = resolved[0]
-p2 = resolved[1]
-p3 = resolved[2]
-
-pt1 = p1.sig.dump.readWord( p1.getAddress() )
-pt1b = p1.sig.dump.readWord( p1.getAddress(1) )
-
-pt1 in p1.sig.dump
-pt1b in p1.sig.dump
-
-pt2 = p2.sig.dump.readWord( p2.getAddress() )
-pt3 = p3.sig.dump.readWord( p3.getAddress() )
-
-ap1 = pattern.AnonymousStructRange(p1)
-len(ap1)
-ap.structLen()
-
-b1 = p1.sig.dump.readBytes( p1.getAddress()-16, 32 )
-cut =32
-print b1.encode('hex')[0:cut],b1.encode('hex')[cut:]
-
-p1.sig.dump.readWord
-
-#b2 = p2.sig.dump.readBytes( p2.getAddress()-16, 32 )
-#b3 = p3.sig.dump.readBytes( p3.getAddress()-16, 32 )
-
-#print b2.encode('hex')[0:cut],b2.encode('hex')[cut:]
-#print b3.encode('hex')[0:cut],b3.encode('hex')[cut:]
+if __name__ == '__main__':
+  main(sys.argv[1:])
 
 
 
-sig = mapper.signatures[0]
-resolved_for_sig = [ pp for pp in mapper.resolved if pp.sig == sig ]
-pinned = [pattern.AnonymousStructRange(pp) for pp in resolved_for_sig]
-print('check for overlapping #not possible# on %d values '%(len(pinned)))
-for i, pp in enumerate(pinned):
-  if pp.stop in pinned[i+1:]:
-    #print('OVERLAPPING PinnedPointers - THAT IS NOT POSSIBLE. WHO MESSED WITH MY CODE !')
-    j = pinned[i+1:].index(pp.stop)
-    c = pinned[i+1+j]
-    #print pp.start, pp.stop, c.start, c.stop,
-    print pp.pinnedPointer, c.pinnedPointer
-    #print pp.stop in c
-    #break
-    
-
-
-
-
-
-
-
-
-
-
-
-
-import itertools
-allpp = sorted([v for l in mapper.cacheValues2.values() for v in l], reverse=True)
-unresolved = []
-
-for k, g in itertools.groupby( allpp ):
-  l = list(g)
-  if len(l) < len(mapper.signatures): # we can have multiple instances btu not less.
-    unresolved.extend(l)
-    #print 'not same numbers'
-    continue
-  else:
-    # we should have all 3 signatures
-    found = [pp.sig for pp in l ]
-    for s in mapper.signatures:
-      if s not in found:
-        unresolved.extend(l)
-        #print 'not same sigs', s
-        break
-
-unresolved.reverse()
-
-print 'left with %d/%d unresolved pp'%(len(unresolved), len(allpp) )
-
-## we are left with pp present in less than all sigs.
-
-a=unresolved[:10]
-for p in a:
-  print p
-
-
-
-print '%s not in found'%(s) 
-print 'we have to find a PinnedPointer related to size %s and %s'%(k, ['%s'%pp for pp in v])
 
