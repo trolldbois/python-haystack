4a24c180cbf4bd3288ea57b8e5711b0508cb33f5
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-08-31 15:23:59 -0400

    move controller out of model

---
+++ haystack/reverse/heuristics/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Loic Jaquemet loic.jaquemet+python@gmail.com
+#
+
+__author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
+
+__doc__ = '''
+  Heuristics to do static type analysis from bytes.
+'''
+
+#import pattern
+
+all =[
+]
+
+
