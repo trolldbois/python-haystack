76a56cab802bb64f76ad4dfaed792d5d4baf8815
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2014-01-30 18:42:24 -0700

    TU for python

--- test/haystack/outputters/test_python.py
+++ test/haystack/outputters/test_python.py
@@ -49,42 +49,109 @@ class TestToPyObject(SrcTests):
         import ctypes
         self.assertEquals(int(self.sizes['struct_d']), ctypes.sizeof(d))
         
-        obj = d.toPyObject()
-        #print obj.toString()
-        import code
-        #print obj.f.toString()
-        #code.interact(local=locals())
-        ## void pointer
-        self.assertEquals(None, obj.a)
-        #
-        self.assertEquals(int(self.values['struct_d.b.e']), obj.b.e)
-        self.assertEquals(int(self.values['struct_d.b2.e']), obj.b2.e)
+        parser = python.PythonOutputter(self.mappings)
+        obj = parser.parse(d)
+
+        self.assertEquals(d.a.value, self.offsets['struct_d'][0])
+        self.assertEquals(d.b.value, self.offsets['struct_d.b'][0])
+        self.assertEquals(d.b2.value, self.offsets['struct_d.b2'][0])
         for i in range(9):
             self.assertEquals(int(self.values['struct_d.c[%d].a'%(i)]), obj.c[i].a)
             self.assertEquals(int(self.values['struct_d.f[%d]'%(i)]), obj.f[i])
         self.assertEquals(int(self.values['struct_d.e']), obj.e)
-        # FIXME: how do you exprime a CString with a POINTER(c_char)
-        #self.assertEquals(str(self.values['struct_d.h']), obj.h)
         self.assertEquals(str(self.values['struct_d.i']), obj.i)
 
         
         return 
 
-    @unittest.expectedFailure
-    def test_CString(self):
-        from test.src import ctypes5_gen32
+    def test_basic_types(self):
+        from test.src import ctypes5_gen32        
         model.registerModule(ctypes5_gen32)
         # struct a - basic types
-        offset = self.offsets['struct_d'][0]
+        offset = self.offsets['struct_a'][0]
         m = self.mappings.getMmapForAddr(offset)
-        d = m.readStruct(offset, ctypes5_gen32.struct_d)
-        ret = d.loadMembers(self.mappings, 10 )
+        a = m.readStruct(offset, ctypes5_gen32.struct_a)
+        ret = a.loadMembers(self.mappings, 10 )
+        self.assertTrue(ret)
+        import ctypes
+        self.assertEquals(int(self.sizes['struct_a']), ctypes.sizeof(a))
 
-        obj = d.toPyObject()
-        # FIXME: how do you exprime a CString with a POINTER(c_char)
-        self.assertEquals(str(self.values['struct_d.h']), obj.h)
+        parser = python.PythonOutputter(self.mappings)
+        a = parser.parse(a)
 
+        self.assertEquals(int(self.values['struct_a.a']), a.a)
+        self.assertEquals(int(self.values['struct_a.b']), a.b)
+        self.assertEquals(int(self.values['struct_a.c']), a.c)
+        self.assertEquals(int(self.values['struct_a.d']), a.d)
+        self.assertEquals(int(self.values['struct_a.e']), a.e)
+        self.assertEquals(float(self.values['struct_a.f']), a.f)
+        self.assertEquals(float(self.values['struct_a.g']), a.g)
+        self.assertEquals(float(self.values['struct_a.h']), a.h)
+
+
+        offset = self.offsets['union_au'][0]
+        m = self.mappings.getMmapForAddr(offset)
+        au = m.readStruct(offset, ctypes5_gen32.union_au)
+        ret = au.loadMembers(self.mappings, 10 )
+        self.assertTrue(ret)
+        au = parser.parse(au)
+        self.assertEquals(int(self.values['union_au.d']), au.d)
+        self.assertEquals(float(self.values['union_au.g']), au.g)
+        self.assertEquals(float(self.values['union_au.h']), au.h)
+        
         return 
+
+    def test_basic_signed_types(self):
+        from test.src import ctypes5_gen32        
+        model.registerModule(ctypes5_gen32)
+        # struct a - basic types
+        offset = self.offsets['union_b'][0]
+        m = self.mappings.getMmapForAddr(offset)
+        b = m.readStruct(offset, ctypes5_gen32.union_b)
+        ret = b.loadMembers(self.mappings, 10 )
+        self.assertTrue(ret)
+        import ctypes
+        parser = python.PythonOutputter(self.mappings)
+        b = parser.parse(b)
+        
+        self.assertEquals(int(self.values['union_b.a']), b.a)
+        self.assertEquals(int(self.values['union_b.b']), b.b)
+        self.assertEquals(int(self.values['union_b.c']), b.c)
+        self.assertEquals(int(self.values['union_b.d']), b.d)
+        self.assertEquals(int(self.values['union_b.e']), b.e)
+        # char 251
+        self.assertEquals((self.values['union_b.g']), b.g)
+        
+        return 
+
+    def test_bitfield(self):
+        from test.src import ctypes5_gen32        
+        model.registerModule(ctypes5_gen32)
+        # struct a - basic types
+        offset = self.offsets['struct_c'][0]
+        m = self.mappings.getMmapForAddr(offset)
+        c = m.readStruct(offset, ctypes5_gen32.struct_c)
+        ret = c.loadMembers(self.mappings, 10 )
+        self.assertTrue(ret)
+
+        import ctypes
+        self.assertEquals(int(self.sizes['struct_c']), ctypes.sizeof(c))
+
+        parser = python.PythonOutputter(self.mappings)
+        c = parser.parse(c)
+
+        self.assertEquals(int(self.values['struct_c.a1']), c.a1)
+        self.assertEquals(int(self.values['struct_c.b1']), c.b1)
+        self.assertEquals(int(self.values['struct_c.c1']), c.c1)
+        self.assertEquals(int(self.values['struct_c.d1']), c.d1)
+        self.assertEquals(str(self.values['struct_c.a2']), c.a2)
+        self.assertEquals(int(self.values['struct_c.b2']), c.b2)
+        self.assertEquals(int(self.values['struct_c.c2']), c.c2)
+        self.assertEquals(int(self.values['struct_c.d2']), c.d2)
+        self.assertEquals(int(self.values['struct_c.h']), c.h)
+        
+        return 
+
 
 
 
