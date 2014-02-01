8a8af37050160094b67d735222eff5735ebb8801
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2014-02-01 13:16:19 -0700

    move TU

---
+++ test/haystack/mappings/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Loic Jaquemet loic.jaquemet+python@gmail.com
+#
+
+__author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
+
+__doc__ = """
+  Memory Mappings used in haystack
+"""
+
+all =[
+]
+
+
