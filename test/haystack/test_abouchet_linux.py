1e3ba853b2b7dd69035b0e6596733bd375861e39
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-01-30 17:26:54 -0700

    extract complicated TU from linux to windows platform

--- test/haystack/test_abouchet_linux.py
+++ test/haystack/test_abouchet_linux.py
@@ -490,45 +490,6 @@ class TestApiLinuxDump(unittest.TestCase):
 
         return 
 
-class TestApiWin32Dump(unittest.TestCase):
-    """ test is the python API works. """
-    def setUp(self):
-        model.reset()
-        self.memdumpname = 'test/dumps/putty/putty.1.dump'
-        self.classname = 'haystack.structures.win32.win7heap.HEAP'
-        self.known_heaps = [ (0x00390000, 8956), (0x00540000, 868),
-                        (0x00580000, 111933), (0x005c0000, 1704080) , 
-                        (0x01ef0000, 604), (0x02010000, 61348), 
-                        (0x02080000, 474949), (0x021f0000 , 18762),
-                        (0x03360000, 604), (0x04030000 , 632),
-                        (0x04110000, 1334), (0x041c0000 , 644),
-                        # from free stuf
-                        (0x0061a000, 1200),
-                        ]
-
-    def tearDown(self):
-        self.mappings = None
-        model.reset()
-
-    def test_show(self):
-        instance, validated = abouchet.show_dumpname(self.classname, self.memdumpname, self.known_heaps[0][0])
-        self.assertTrue(validated)
-        self.assertIsInstance(instance, object)
-        self.assertEquals(instance.Signature, 0xeeffeeff)
-        self.assertEquals(instance.VirtualMemoryThreshold, 0xfe00)
-        self.assertEquals(instance.FrontEndHeapType, 0)
-
-        if False:
-            instance, validated = abouchet.show_dumpname(self.classname, self.memdumpname, self.known_heaps[0][0]+1)
-            self.assertFalse(validated)
-            self.assertIsInstance(instance, object)
-            self.assertNotEquals(instance.Signature, 0xeeffeeff)
-            self.assertEquals(   instance.Signature, 0xeeffee) # 1 byte off
-            self.assertNotEquals(instance.VirtualMemoryThreshold, 0xfe00)
-            self.assertEquals(   instance.VirtualMemoryThreshold, 0xff0000fe)
-
-        return 
-
 
 
 if __name__ == '__main__':
