2b239090f9ffbd75072c9a7786ca730e6d5c83d9
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-05-30 00:27:53 -0600

    version management

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -29,6 +29,22 @@ __all__ = [
   'refresh'
 ]
 
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+
+try:
+    _dist = get_distribution('haystack')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'haystack')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+
 # in any haystack import, we need to ensure the model is loaded.
 import model
 
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __maintainer__ = "Loic Jaquemet"
 __status__ = "Production"
 
 setup(name="haystack",
-    version="0.19-pre",
+    version="0.20",
     description="Search C Structures in a process' memory",
     long_description=open("README.rst").read(),
 
