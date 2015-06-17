99bda5433f1f07971a47330cebc887a26b3b76de
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-06-16 18:33:31 -0600

    pep8

--- test/src/make.py
+++ test/src/make.py
@@ -12,20 +12,20 @@ from haystack import memory_dumper
 log=logging.getLogger('make.py')
 
 def main():
-  dumpname = sys.argv[1]
-  fn = file('/dev/null')
-  app = './%s'%(dumpname[:dumpname.rindex('.')])
-  try:
-    shutil.rmtree(dumpname)
-  except:
-    pass
-  out = file(app+".stdout",'w')
-  #pid1 = subprocess.Popen([app], stdout=fn.fileno())
-  pid1 = subprocess.Popen([app], bufsize=-1, stdout=out.fileno())
-  time.sleep(0.9) # 
-  #print('   **DUMP** ', pid1.pid)
-  memory_dumper.dump(pid1.pid, dumpname)
-  pid1.kill()
+    dumpname = sys.argv[1]
+    fn = file('/dev/null')
+    app = './%s'%(dumpname[:dumpname.rindex('.')])
+    try:
+        shutil.rmtree(dumpname)
+    except:
+        pass
+    out = file(app+".stdout",'w')
+    #pid1 = subprocess.Popen([app], stdout=fn.fileno())
+    pid1 = subprocess.Popen([app], bufsize=-1, stdout=out.fileno())
+    time.sleep(0.9) # 
+    #print('   **DUMP** ', pid1.pid)
+    memory_dumper.dump(pid1.pid, dumpname)
+    pid1.kill()
 
 
 if __name__ == '__main__':
