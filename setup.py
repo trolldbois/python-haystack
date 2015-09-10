85bb7b23de2a2f60e9ea98c3e94d35932e902321
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-09-10 14:45:00 -0600

    updated to v0.22

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class PyPrepTestsCommand(distutils.cmd.Command):
 
 
 setup(name="haystack",
-      version="0.21",
+      version="0.22",
       description="Search C Structures in a process' memory",
       long_description=open("README.md").read(),
       url="http://packages.python.org/haystack/",
