0294306ed645c94acac23e68e9ffbb0b3b29a80e
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-11-11 16:31:08 -0700

    Fixing the memory mapper factory

--- haystack/memory_mapper.py
+++ haystack/memory_mapper.py
@@ -1,31 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Gets memory _memory_handler from a PID or a haystack dump."""
-
 import logging
 import time
 
-import os
-
-import haystack
-from haystack.dbg import PtraceDebugger
+from haystack import dbg
 from haystack import dump_loader
 from haystack.abc import interfaces
-from haystack.mappings import base
-from haystack.mappings import file
 from haystack.mappings import process
 from haystack.mappings import vol
 from haystack.mappings import rek
 
 log = logging.getLogger('mapper')
-
-__author__ = "Loic Jaquemet"
-__copyright__ = "Copyright (C) 2012 Loic Jaquemet"
-__email__ = "loic.jaquemet+python@gmail.com"
-__license__ = "GPL"
-__maintainer__ = "Loic Jaquemet"
-__status__ = "Production"
 
 
 class MemoryHandlerFactory(interfaces.IMemoryLoader):
@@ -35,85 +21,42 @@ class MemoryHandlerFactory(interfaces.IMemoryLoader):
     def __init__(self, pid=None, mmap=True, memfile=None,
                  baseOffset=None, dumpname=None, volname=None, rekallname=None):
         memory_handler = None
-        if not (volname is None) and not (pid is None):
-            memory_handler = self._init_volatility(volname, "WinXPSP2x86", pid)
-        elif not (rekallname is None) and not (pid is None):
-            memory_handler = self._init_rekall(rekallname, pid)
+        if volname is not None and pid is not None:
+            mapper = vol.VolatilityProcessMapper(volname, "WinXPSP2x86", pid)
+            memory_handler = mapper.make_memory_handler()
+        elif rekallname is not None and pid is not None:
+            mapper = rek.RekallProcessMapper(volname, pid)
+            memory_handler = mapper.make_memory_handler()
+        elif not (dumpname is None):
+            loader = dump_loader.ProcessMemoryDumpLoader(dumpname)
+            memory_handler = loader.make_memory_handler()
         elif not (pid is None):
             memory_handler = self._init_pid(pid, mmap)
-        elif not (memfile is None):
-            memory_handler = self._init_memfile(memfile, baseOffset)
-        elif not (dumpname is None):
-            memory_handler = self._init_process_dumpfile(dumpname)
         self.__memory_handler = memory_handler
         return
 
     def make_memory_handler(self):
-        """Creates a MemoryHandler
+        """
+        Returns the instanciated MemoryHandler
 
         :rtype : IMemoryHandler
         """
         return self.__memory_handler
 
     @staticmethod
-    def _init_process_dumpfile(dumpname):
-        loader = dump_loader.ProcessMemoryDumpLoader(dumpname)
-        mappings = loader.make_memory_handler()
-        return mappings
-
-    @staticmethod
-    def _init_memfile(memfile, baseOffset):
-        size = os.fstat(memfile.fileno()).st_size
-        if size > haystack.MAX_MAPPING_SIZE_FOR_MMAP:
-            mem = file.FileBackedMemoryMapping(
-                memfile,
-                baseOffset,
-                baseOffset +
-                size)  # is that valid ?
-            log.warning(
-                'Dump file size is big. Using file backend memory mapping. Its gonna be slooow')
-        else:
-            mem = file.MemoryDumpMemoryMapping(
-                memfile,
-                baseOffset,
-                baseOffset +
-                size)  # is that valid ?
-        mappings = base.MemoryHandler([mem], memfile.name)
-        return mappings
-
-    @staticmethod
     def _init_pid(pid, mmap):
         if not isinstance(pid, (int, long)):
             raise TypeError('PID should be a number')
-        dbg = PtraceDebugger()
-        my_process = dbg.open_process(pid, is_attached=False)
-        if my_process is None:
-            log.error("Error initializing Process debugging for %d" % pid)
-            raise IOError
-            # ptrace exception is raised before that
-        _memory_handler = process.readProcessMappings(my_process)
+        my_debugger = dbg.get_debugger(pid)
+        _memory_handler = process.readProcessMappings(my_debugger.get_process())
         t0 = time.time()
         for m in _memory_handler:
             if mmap:
-                # mmap memory in local space
+                # force to mmap the memory in local space
                 m.mmap()
                 log.debug('mmap() : %d' % (len(m.mmap())))
         if mmap:
             # mmap done, we can release process...
-            my_process.cont()
-            log.info(
-                'MemoryHandler mmaped, process released after %02.02f secs' %
-                (time.time() - t0))
-        return _memory_handler
-
-    @staticmethod
-    def _init_volatility(volname, profile, pid):
-        mapper = vol.VolatilityProcessMapper(volname, profile, pid)
-        _memory_handler = mapper.make_memory_handler()
-        return _memory_handler
-
-    @staticmethod
-    def _init_rekall(volname, pid):
-        mapper = rek.RekallProcessMapper(volname, pid)
-        _memory_handler = mapper.make_memory_handler()
+            my_debugger.get_process().resume()
+            log.info('MemoryHandler mmaped, process released after %02.02f secs', time.time() - t0)
         return _memory_handler
