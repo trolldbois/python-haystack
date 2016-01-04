cbdc0d528084629225a19fc18f43f488c7473aa6
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2016-01-04 03:39:10 -0700

    better addresses in text output

--- haystack/outputters/text.py
+++ haystack/outputters/text.py
@@ -27,14 +27,19 @@ class RecursiveTextOutputter(Outputter):
     Returned string should be python compatible ( dict )
     """
 
-    def parse(self, obj, prefix='', depth=10):
+    def parse(self, obj, prefix='', depth=10, addr_was=None):
         """ Returns a string formatted description of this Structure.
         The returned string should be python-compatible...
         """
         # TODO: use a ref table to stop loops on parsed instance,
         #             depth kinda sux.
         # if obj._orig_address_ in self._addr_cache:
-        addr = self._ctypes.addressof(obj)
+        if addr_was:
+            addr = addr_was
+        elif hasattr(obj, '_orig_address_'):
+            addr = obj._orig_address_
+        else:
+            addr = self._ctypes.addressof(obj)
         if addr in self._addr_cache:
             if type(obj) in self._addr_cache[addr]:
                 #depth = min(depth, 1)
@@ -52,9 +57,10 @@ class RecursiveTextOutputter(Outputter):
             s = "{ # <%s at 0x%x>" % (
                 obj.__class__.__name__, obj._orig_address_)
         else:
-            s = "{ # <%s at 0x???>" % (obj.__class__.__name__)
+            s = "{ # <%s at 0x%x>" % (obj.__class__.__name__, addr)
         for field, typ in basicmodel.get_fields(obj):
             attr = getattr(obj, field)
+            offset = getattr(type(obj), field).offset
             s += '\n%s"%s": %s' % (prefix,
                                    field,
                                    self._attrToString(
@@ -62,12 +68,13 @@ class RecursiveTextOutputter(Outputter):
                                        field,
                                        typ,
                                        prefix,
+                                       addr + offset,
                                        depth))
         s += '\n' + prefix + '}'
         self._addr_cache = {}
         return s
 
-    def _attrToString(self, attr, field, attrtype, prefix, depth=-1):
+    def _attrToString(self, attr, field, attrtype, prefix, addr, depth=-1):
         """This should produce strings, based on self._ctypes allocators. No pyOBJ"""
         s = ''
         if self._ctypes.is_basic_type(attrtype):
@@ -84,12 +91,12 @@ class RecursiveTextOutputter(Outputter):
             except TypeError as e:
                 pass
         elif self._ctypes.is_struct_type(attrtype) or self._ctypes.is_union_type(attrtype):
-            s = '%s,' % (self.parse(attr, prefix + '\t', depth - 1))
+            s = '%s,' % (self.parse(attr, prefix + '\t', depth - 1, addr_was=addr))
         elif self._ctypes.is_function_type(attrtype):
             # only print address in target space
             myaddress = self._utils.get_pointee_address(attr)
             myaddress_fmt = self._utils.formatAddress(myaddress)
-            s = '%s, #(FIELD NOT LOADED: function type)' % (myaddress_fmt)
+            s = '%s, #(FIELD NOT LOADED: function type)' % myaddress_fmt
         elif self._ctypes.is_array_of_basic_type(attrtype):
             # array of int, float, char...
             s = '%s,' % (repr(self._utils.ctypes_to_python_array(attr)))
@@ -98,14 +105,17 @@ class RecursiveTextOutputter(Outputter):
             # go through each elements, we hardly can make a array out of that.
             s = '['
             _attrType = self._utils.get_subtype(attrtype)
+            _size = self._ctypes.sizeof(_attrType)
             # eltyp = type(attr[0])
             for i in range(0, len(attr)):
+                _addr = addr + i*_size
                 s += '\n%s%s' % (prefix + '\t',
                                  self._attrToString(
                                      attr[i],
                                      i,
                                      _attrType,
                                      prefix,
+                                     _addr,
                                      depth - 1))
             s += '\n%s],' % (prefix + '\t')
         elif self._ctypes.is_cstring_type(attrtype):
@@ -132,7 +142,7 @@ class RecursiveTextOutputter(Outputter):
             #    code.interact(local=locals())
             if myaddress == 0 or contents is None: # FIXME the solution is probably to remove the content test here
                 # only print address/null
-                s = '%s,' % (myaddress_fmt)
+                s = '%s,' % myaddress_fmt
             elif self._ctypes.is_pointer_to_void_type(attrtype):
                 # c_void_p, c_char_p, can load target
                 s = '%s, #(FIELD NOT LOADED: void pointer)' % myaddress_fmt # self._utils.formatAddress(attr.value)
@@ -146,6 +156,8 @@ class RecursiveTextOutputter(Outputter):
             elif (self._ctypes.is_pointer_to_struct_type(attrtype)
                   or self._ctypes.is_pointer_to_union_type(attrtype)):
                 s = '%s,' % (self.parse(contents, prefix + '\t', depth - 1))
+                # s = '%s# %s\n%s%s,' % (prefix, myaddress_fmt, prefix, self.parse(contents, prefix + '\t', depth - 1))
+
             # FIXME: get coherent with python or just use the one python and then a python to string.
             else:
                 # FIXME we should NOT recurse
